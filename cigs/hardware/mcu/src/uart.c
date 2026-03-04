0d31d2544c76754a8add332818a6efc000ef0a8c
B25A5006 <s25A5006PM@chibatech.ac.jp>
2026-03-04 13:58:01 +0900

    12バイトのコマンドには正常に動作

--- cigs/hardware/mcu/src/uart.c
+++ cigs/hardware/mcu/src/uart.c
@@ -1,6 +1,8 @@
 #INT_RDA3
 volatile unsigned int8 boss_receive_buffer[RECEIVE_BUFFER_MAX] = {0x00};
 volatile int8 boss_receive_buffer_size = 0;
+
+/*/
 static void RDA_isr(void)
 {
     // ハードウェアがデータを受信した瞬間に自動で呼ばれる
@@ -14,12 +16,13 @@ static void RDA_isr(void)
         fgetc(BOSS); // バッファ満杯時は捨てる
     }
 }
+*/
 
 void setup_uart_to_boss()
 {
     fprintf(PC, "UART Initialize\r\n");
-    enable_interrupts(INT_RDA3);
-    enable_interrupts(GLOBAL);
+    //enable_interrupts(INT_RDA3);
+    //enable_interrupts(GLOBAL);
     fprintf(PC, "\tComplete\r\n");
 }
 
--- son_tmp_main.c
+++ son_tmp_main.c
@@ -1,5 +1,3 @@
-//#include "cigs/system/son_tmp_main.h"
-
 #include "cigs/system/son_tmp_main.h"
 
 void main()
@@ -10,39 +8,66 @@ void main()
     fprintf(PC,"Last updated on %s %s.\r\n\r\n", __DATE__, __TIME__);
 
     io_init();
-    setup_uart_to_boss(); // ここで割り込みがオンになる
+    setup_uart_to_boss(); // 割り込みはオフになっています
     setup_timer();
     misf_init();
 
     piclog_make(0x00, 0x00);
 
     int1 is_finished = FALSE;
-    int8 last_buffer_size = 0; // デバッグ用：前回のバッファサイズを記憶
+    int8 last_buffer_size = 0;
+    int32 loop_counter = 0;
 
     fprintf(PC,"____TMP PIC Start Operation_____\r\n\r\n");
     fprintf(PC,"waiting for BOSS PIC command\r\n");
 
     while(!is_finished)
     {
+        // 約1秒ごとにピリオドを打って、フリーズしていないことを証明する
+        loop_counter++;
+        if (loop_counter > 20000) {
+            fprintf(PC, ".");
+            loop_counter = 0;
+        }
+
+        // --- 1. ハードウェアUARTのバッファから安全にデータを吸い上げる ---
+        if (kbhit(BOSS))
+        {
+            uint8_t timeout_ms = 0;
+            // データが途切れるまで全力で吸い上げる
+            while(timeout_ms < 5)
+            {
+                if (kbhit(BOSS)) {
+                    if (boss_receive_buffer_size < RECEIVE_BUFFER_MAX) {
+                        boss_receive_buffer[boss_receive_buffer_size++] = fgetc(BOSS);
+                    } else {
+                        fgetc(BOSS); // 満杯時は捨てる
+                    }
+                    timeout_ms = 0;
+                } else {
+                    delay_ms(1);
+                    timeout_ms++;
+                }
+            }
+        }
+        // ---------------------------------------------------------------
+
         // --- デバッグ機能：受信状況のリアルタイム表示 ---
-        // バッファのサイズが増えた（データを受信した）ら表示する
         if (boss_receive_buffer_size != last_buffer_size)
         {
-            fprintf(PC, "\r\n[DEBUG] RX(%d/12): ", boss_receive_buffer_size);
+            fprintf(PC, "\r\n[DEBUG] RX(%d): ", boss_receive_buffer_size);
             for(int i = 0; i < boss_receive_buffer_size; i++)
             {
                 fprintf(PC, "%02X ", boss_receive_buffer[i]);
             }
-            last_buffer_size = boss_receive_buffer_size; // サイズを更新
+            last_buffer_size = boss_receive_buffer_size;
         }
         // ------------------------------------------------
 
-        // パケットの長さ(12バイト)以上貯まったら処理を開始する
+        // 12バイト貯まったらパース処理
         if(boss_receive_buffer_size >= 12)
         {
             Command recieve_cmd = make_receive_command((uint8_t*)boss_receive_buffer, boss_receive_buffer_size);
-
-            // 処理が終わったらバッファをクリアする
             clear_receive_signal((uint8_t*)boss_receive_buffer, &boss_receive_buffer_size);
 
             if(recieve_cmd.is_exist)
@@ -50,21 +75,19 @@ void main()
                 fprintf(PC, "\r\n[INFO] Valid Command Frame Received! ID: %02X\r\n", recieve_cmd.frame_id);
                 execute_mission_command(&recieve_cmd);
 
-                // 次のコマンドに備えてバッファを完全に空にする
                 boss_receive_buffer_size = 0;
-                last_buffer_size = 0; // デバッグ変数もリセット
+                last_buffer_size = 0;
                 fprintf(PC,"\r\nwaiting for BOSS PIC command\r\n");
             }
             else
             {
                 fprintf(PC, "\r\n[WARN] Invalid Frame. Discarding buffer.\r\n");
                 boss_receive_buffer_size = 0;
-                last_buffer_size = 0; // デバッグ変数もリセット
+                last_buffer_size = 0;
             }
         }
 
-        if(is_finished == TRUE)
-            break;
+        if(is_finished == TRUE) break;
 
         delay_us(50);
     }
@@ -81,6 +104,7 @@ void main()
 }
 
 /*
+#include "cigs/system/son_tmp_main.h"
 void main()
 {
     delay_ms(100); // wait for power stable
